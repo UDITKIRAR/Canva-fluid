c4d6c0ca7d1a2dbb718907eafe33eaee42076c86
unknown <uditkirar02u@example.com>
2024-05-19 15:38:59 +0530

    cteed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -24,17 +24,17 @@ export const Navbar = () => {
               <div className="mt-3 text-custom-purple w-full ">
                 <p className=" text-lg lg:whitespace-nowrap text-[#b8b0c9]  ">
                   Motion Art for Elementor is a groundbreaking plugin that
-                  empowers you to effortlessly infuse{" "}
+                  empowers you to effortlessly infuse
                 </p>
                 <p className=" text-lg text-[#b8b0c9]  lg:whitespace-nowrap  ">
-                  your website with visually stunning motion art elements.{" "}
+                  your website with visually stunning motion art elements.
                 </p>
               </div>
             </div>
           </div>
           <div className="flex  z-100 relative justify-between items-center w-full px-3">
             <div className="border-3px   rounded-md w-full h-auto flex justify-center md:block mt-4">
-              <div className="p-4 pointer-event-auto relative px-18 inline-flex justify-center items-center gap-6  bg-gradient-to-r from-indigo-800 to-orange-500 rounded-2xl px-8">
+              <a  href="https://codecanyon.net/item/motion-art-for-elementor-wordpress-plugin/48826891" className="p-4 pointer-event-auto relative px-18 inline-flex justify-center items-center gap-6  bg-gradient-to-r from-indigo-800 to-orange-500 rounded-2xl px-8">
                 <div className=" ">
                   <a
                     className="text-white text-xl "
@@ -46,7 +46,7 @@ export const Navbar = () => {
                 <div>
                   <FaArrowRightLong className="text-white text-xl font-thin" />
                 </div>
-              </div>
+              </a>
             </div>
           </div>
         </div>
