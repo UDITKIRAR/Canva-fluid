14e485f69c796e2a1f367a22ec3ab86289835f69
unknown <uditkirar02u@example.com>
2024-05-20 23:08:50 +0530

    fnamlt

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,7 +13,7 @@ import { FaArrowRightLong } from "react-icons/fa6";
 export const Navbar = () => {
   return (
     <div className="flex flex-col mt-4 ">
-      <nav className="order-3 flex  flex-col md:flex-row md:justify-between  md:mt-22   ">
+      <nav className="order-3 flex  flex-col md:flex-row md:justify-between  md:mt-20   ">
         <div className="flex flex-col-reverse  ">
           <div className={styles.navbar3}>
             <div className=" mt-10 pointers-event-auto  p-4 flex flex-col gap-4 col-span-3">
